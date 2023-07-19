c33fb717edba71723326289294548288ce0c3024
Adauto <adauto.matosjr@gmail.com>
2023-07-18 23:55:45 -0300

    categoria do feed

--- laberango/src/Pages/Feed/feed.js
+++ laberango/src/Pages/Feed/feed.js
@@ -5,56 +5,73 @@ import { ContainerFeed, CardResta, InputFeed, Menu, MenuItens } from "./styled";
 import CardRestaurant from "../../Components/cardRestaurant/cardRestaurant";
 import { Header } from "../../Components/Headers/header";
 
-export const Feed = () =>{
-    const [restaurants, setRestaurants] = useState([])
-    const [inputSearch, setInputSearch] = useState('')
-    const getRestaurants = () => {
-        axios
-        .get(`${BASE_URL}/restaurants`,{
-            headers:{
-                auth: localStorage.getItem("token")
-             }
-            })
-        .then((res) => {
-            console.log(res.data)
-            setRestaurants(res.data.restaurants)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-    }
-    console.log(inputSearch)
-    useEffect(() =>{
-        getRestaurants()
-    }, [])
-    const filterRestaurant = restaurants.filter((restaurant) => {
-        return inputSearch ? restaurant.name.toLowerCase().includes(inputSearch.toLowerCase()) : true;
-      }).map((restaurant) =>{
-        return <CardRestaurant restaurant={restaurant}/>
-     })
-      
+export const Feed = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [inputSearch, setInputSearch] = useState("");
+  const [categoriaRestaurant, setCategoriaRestaurant] = useState([]);
 
-   
-    return (
+  const getRestaurants = () => {
+    axios
+      .get(`${BASE_URL}/restaurants`, {
+        headers: {
+          auth: localStorage.getItem("token")
+        }
+      })
+      .then((res) => {
+        console.log(res.data);
+        setRestaurants(res.data.restaurants);
+        filterCategoria(res.data.restaurants);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const filterCategoria = (restaurants) => {
+    const arrayAux = [];
+    restaurants.forEach((res) => {
+      arrayAux.push(res.category);
+    });
+
+    const arrayTiraRepet = [...new Set(arrayAux)];
+
+    setCategoriaRestaurant(arrayTiraRepet);
+    console.log(arrayTiraRepet);
+  };
+
+  console.log(inputSearch);
+
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  const filterRestaurant = restaurants
+    .filter((restaurant) => {
+      return inputSearch
+        ? restaurant.name.toLowerCase().includes(inputSearch.toLowerCase())
+        : true;
+    })
+    .map((restaurant) => {
+      return <CardRestaurant restaurant={restaurant} />;
+    });
+
+  return (
     <ContainerFeed>
-        <Header title={"LabeRango"}/>
-        <InputFeed
-            value={inputSearch}
-            onChange={(event)=>setInputSearch(event.target.value)}
-        />
-        <Menu>
-            <MenuItens select ={true}>Burguer</MenuItens>
-            <MenuItens select ={false}>Massas</MenuItens>
-            <MenuItens select ={false}>Assiática</MenuItens>
-            <MenuItens select ={false}>Saudável</MenuItens>
-            <MenuItens select ={false}>Promoção</MenuItens>
-            <MenuItens select ={false}>Bebidas</MenuItens>        
-        </Menu>
-        <CardResta>
-             {filterRestaurant}
-        </CardResta>
+      <Header title={"LabeRango"} />
+      <InputFeed
+        value={inputSearch}
+        onChange={(event) => setInputSearch(event.target.value)}
+      />
+      <Menu>
+        {categoriaRestaurant.map((categoria, index) => (
+          <MenuItens key={index} select={false}>
+            {categoria}
+          </MenuItens>
+        ))}
+      </Menu>
+      <CardResta>{filterRestaurant}</CardResta>
     </ContainerFeed>
-    )
-}
+  );
+};
 
-export default Feed 
+export default Feed;
